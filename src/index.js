ac7cce631da58cbd0c93f6dcf73b3eb8b56faca5
sameh <sameh080081@gmail.com>
2022-07-15 16:23:09 +0400

    update functionality

--- src/index.js
+++ src/index.js
@@ -1,26 +1,73 @@
 import './style.css';
 
 const list = document.querySelector('.list');
+const clear = document.querySelector('.clear');
+const enter = document.querySelector('.enter');
+const tasks = document.querySelectorAll('.edit');
+const input = document.querySelector('.task_input');
+const array = JSON.parse(localStorage.getItem('array')) || [];
 
-const array = [
-  {
-    description: 'clean house',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'feed animals',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'go shopping',
-    completed: false,
-    index: 2,
-  },
-];
+enter.addEventListener('click', (e) => {
+  add();
+});
 
-array.forEach((task) => {
-  const item = `<li class="item"><div><input type="checkbox">${task.description}</div><a class="move" href="#"><img alt= "move" src="../../../icons/vector3.png"></a></li>`;
+input.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+    add();
+  }
+});
+
+function add () {
+  if(input.value != ''){
+   array.push({description: input.value,
+   completed: false});
+   localStorage.setItem('array',JSON.stringify(array));
+   input.value = '';
+   display();
+  }
+}
+
+function update(index, value) {
+  if(value != ''){
+    array[index] = {description: value, completed: false};
+    localStorage.setItem('array',JSON.stringify(array));
+    display();
+   }
+}
+
+function remove_task(index) {
+  array.splice(index,1);
+  localStorage.setItem('array',JSON.stringify(array));
+  display();
+}
+
+function display(){
+  list.innerHTML=``;
+  array.forEach((task) => {
+  const item = `<li class="item"><div class ="check-cont"><input class="checkbox" type="checkbox">
+  <input class="edit" id="${array.indexOf(task)}" type="text" value = "${task.description}"></div>
+  <a class="delete hide" href="#"><img alt= "delete" src="../../../icons/bin.png"></a>
+  <a class="move" href="#"><img alt= "move" src="../../../icons/vector3.png"></a></li>`;
   list.insertAdjacentHTML('beforeend', item);
 });
+document.querySelectorAll('.edit').forEach((task) => {
+  const remove = task.parentElement.nextElementSibling;
+  task.onfocus = function (){
+    remove.classList.remove('hide');
+  }
+
+  remove.addEventListener('click', (e) => {
+    const index = e.target.parentElement.previousElementSibling.firstChild.nextElementSibling.getAttribute(('id'), 10);
+    remove_task(index);
+  });
+
+  task.addEventListener("keyup", (e) => {
+    if (e.key === "Enter") {
+      const index = parseInt(e.target.getAttribute('id'), 10);
+      update(index, task.value);
+    }
+  });
+});
+}
+
+document.addEventListener('DOMContentLoaded', display());

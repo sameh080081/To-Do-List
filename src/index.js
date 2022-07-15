38c7f7704b520147b042335afb5ccccfbe14f062
sameh <sameh080081@gmail.com>
2022-07-15 17:03:33 +0400

    fix linters

--- src/index.js
+++ src/index.js
@@ -5,33 +5,33 @@ const enter = document.querySelector('.enter');
 const input = document.querySelector('.task_input');
 const array = JSON.parse(localStorage.getItem('array')) || [];
 
-function display(){
-  list.innerHTML=``;
+function display() {
+  list.innerHTML = ``;
   array.forEach((task) => {
-  const item = `<li class="item"><div class ="check-cont"><input class="checkbox" type="checkbox">
-  <input class="edit" id="${array.indexOf(task)}" type="text" value = "${task.description}"></div>
-  <a class="delete hide" href="#"><img alt= "delete" src="../../../icons/bin.png"></a>
-  <a class="move" href="#"><img alt= "move" src="../../../icons/vector3.png"></a></li>`;
-  list.insertAdjacentHTML('beforeend', item);
-});
-document.querySelectorAll('.edit').forEach((task) => {
-  const remove = task.parentElement.nextElementSibling;
-  task.onfocus = function (){
-    remove.classList.remove('hide');
-  }
-
-  remove.addEventListener('click', (e) => {
-    const index = e.target.parentElement.previousElementSibling.firstChild.nextElementSibling.getAttribute(('id'), 10);
-    remove_task(index);
+    const item = `<li class="item"><div class ="check-cont"><input class="checkbox" type="checkbox">
+    <input class="edit" id="${array.indexOf(task)}" type="text" value = "${task.description}"></div>
+    <a class="delete hide" href="#"><img alt= "delete" src="../../../icons/bin.png"></a>
+    <a class="move" href="#"><img alt= "move" src="../../../icons/vector3.png"></a></li>`;
+    list.insertAdjacentHTML('beforeend', item);
   });
-
-  task.addEventListener("keyup", (e) => {
-    if (e.key === "Enter") {
-      const index = parseInt(e.target.getAttribute('id'), 10);
-      update(index, task.value);
+  document.querySelectorAll('.edit').forEach((task) => {
+    const remove = task.parentElement.nextElementSibling;
+    task.onfocus = function (){
+      remove.classList.remove('hide');
     }
+
+    remove.addEventListener('click', (e) => {
+      const index = e.target.parentElement.previousElementSibling.firstChild.nextElementSibling.getAttribute(('id'), 10);
+      remove_task(index);
+    });
+
+    task.addEventListener("keyup", (e) => {
+      if (e.key === "Enter") {
+        const index = parseInt(e.target.getAttribute('id'), 10);
+        update(index, task.value);
+      }
+    });
   });
-});
 }
 
 function add() {

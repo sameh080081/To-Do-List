fd3f29087d3ab353c8ac7ee7d54e15ca2efe373c
sameh <sameh080081@gmail.com>
2022-07-22 05:29:31 +0400

    testing

--- src/index.test.js
+++ src/index.test.js
@@ -1,13 +1,12 @@
 const myModule = require('./methods.js');
 
 let array = [];
-let input = {};
 describe('add and remove', () => {
   // Add
   test('add test', () => {
-    array = myModule.add(array, "test1");
+    array = myModule.add(array, 'test1');
     expect(array).toHaveLength(1);
-    array = myModule.add(array, "test2");
+    array = myModule.add(array, 'test2');
     expect(array).toHaveLength(2);
   });
   test('remove test', () => {
@@ -19,12 +18,12 @@ describe('add and remove', () => {
     expect(array[0].completed).toBe(true);
   });
   test('clear All checked', () => {
-    array = myModule.add(array, "test2");
-    array = myModule.add(array, "test3");
+    array = myModule.add(array, 'test2');
+    array = myModule.add(array, 'test3');
     array = myModule.changeCheck(array, 2, true);
     array = myModule.clearChecked(array);
     expect(array).toHaveLength(1);
     expect(array[0].completed).toBe(false);
-    expect(array[0].description).toBe("test2");
+    expect(array[0].description).toBe('test2');
   });
 });
